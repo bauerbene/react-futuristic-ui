699986478891fab43b56f9401a1774a7bb4b57e6
Benedikt Bauer <benedikt.bauer@posteo.de>
2022-04-30 12:18:46 +0200

    change rollup config for .scss module support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
 const packageJson = require('./package.json');
 
@@ -10,24 +11,26 @@ export default [
         input: 'src/index.ts',
         output: [
             {
-                file: packageJson.main,
-                format: 'cjs',
-                sourceMap: true
-            }, 
-            {
                 file: packageJson.module,
-                format: 'cjs',
+                format: 'esm',
                 sourceMap: true
-            },
+            }
         ],
         plugins: [
             resolve(),
             commonjs(),
-            typescript({ tsconfig: './tsconfig.json' })
+            typescript({
+                tsconfig: './tsconfig.json'
+            }),
+            postcss({
+                sourceMap: true,
+                extract: false,
+                minimize: true
+            })
         ],
     },
     {
-        input: 'dist/esm/types/index.d.ts',
+        input: 'dist/types/index.d.ts',
         output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [
             dts()
